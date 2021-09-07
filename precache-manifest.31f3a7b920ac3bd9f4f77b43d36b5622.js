745bf893c02051ca8100e94bbe9fc491fd49d2b0
alfaroqi <roqialfaroqi@mail.com>
2021-09-07 16:16:36 +0700

    Updates

---
+++ precache-manifest.31f3a7b920ac3bd9f4f77b43d36b5622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33edbe0ec9e59f7a3d9e",
+    "url": "/keyshop/static/js/runtime~main.84b4e045.js"
+  },
+  {
+    "revision": "aa60af84670bec953f2e",
+    "url": "/keyshop/static/js/main.653fe150.chunk.js"
+  },
+  {
+    "revision": "26ddff4a4f4dffd32c6b",
+    "url": "/keyshop/static/js/2.f0652c21.chunk.js"
+  },
+  {
+    "revision": "aa60af84670bec953f2e",
+    "url": "/keyshop/static/css/main.0b1d2bae.chunk.css"
+  },
+  {
+    "revision": "26ddff4a4f4dffd32c6b",
+    "url": "/keyshop/static/css/2.85708ebe.chunk.css"
+  },
+  {
+    "revision": "dbf13ba49019bb72b6cd196fcea56072",
+    "url": "/keyshop/index.html"
+  }
+];
